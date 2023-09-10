034aca0cfc583a1f934618e9a5f3fd476e5aa266
Hermitao <vkriegergranemann@gmail.com>
2023-09-10 18:32:58 -0300

    Added gravity

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,18 @@ use crossterm::{
     event,
     queue,
     terminal::{ClearType, Clear},
-    cursor,
+    cursor, 
 };
 
 
 struct Position {
-    x: u16,
-    y: u16,
+    x: f32,
+    y: f32,
+}
+
+struct Velocity {
+    x: f32,
+    y: f32,
 }
 
 struct Player {
@@ -42,8 +47,7 @@ fn print_events() -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let map = r#"
-####################################################################
+    let map = r#"####################################################################
 #..................................................................#
 #..................................................................#
 #..................................................................#
@@ -72,10 +76,19 @@ fn main() -> std::io::Result<()> {
 ####################################################################
     "#;
 
-    let player_pos = Position {
-        x: 4,
-        y: 4,
+    let mut player_pos = Position {
+        x: 4.0,
+        y: 25.0,
     };
+
+    let mut player_velocity = Velocity {
+        x: 8.0,
+        y: -50.0,
+    };
+
+
+    let gravity: f32 = 60.0;
+    let ground_pos = 26;
 
     let mut stdout = stdout();
    
@@ -89,7 +102,7 @@ fn main() -> std::io::Result<()> {
         print_events();
     });
 
-    const target_fps: f32 = 12.0;
+    const target_fps: f32 = 120.0;
 
     let mut last_frame_time = Instant::now();
     let mut timer: f32 = 0.0;
@@ -106,13 +119,29 @@ fn main() -> std::io::Result<()> {
 
         if timer >= 1.0 / target_fps
         {
+
+            player_velocity.y += gravity * timer;
+
+            player_pos.x += player_velocity.x * timer;
+            player_pos.y += player_velocity.y * timer;
+
+            if (player_pos.y as u16 > ground_pos - 1)
+            {
+                player_pos.y = (ground_pos - 1) as f32;
+                player_velocity.y = 0.0;
+            }
+
             queue!(
                 stdout,
                 Clear(ClearType::All),
                 cursor::MoveTo(0, 0),
                 Print(map),
-                cursor::MoveTo(player_pos.x, player_pos.y),
+                cursor::MoveTo(player_pos.x as u16, player_pos.y as u16),
                 Print("@"),
+                cursor::MoveTo(90, 0),
+                Print(player_pos.y),
+                cursor::MoveTo(90, 1),
+                Print(timer),
             );
 
             stdout.flush();
